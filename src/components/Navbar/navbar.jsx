37f6192f453a3fe4dca66393277fcb4eed45cef9
varun-developer-public <varundev.bytes@gmail.com>
2025-08-06 11:08:39 +0530

    name change

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -5,7 +5,7 @@ function NavBar(){
             <h1>My Website</h1>
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Vimal</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
                 </ul>
